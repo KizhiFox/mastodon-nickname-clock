b67be2b19eebd8e628e17aa805864e34436e73ca
kizhifox@gmail.com <atremchuk@gmail.com>
2022-12-01 12:03:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# In-nickname clock for Mastodon
+
+This script adds the clock to your Mastodon username that shows the current time!
+
+![Username screenshot](username_screenshot.png)
+
+Usage: `clock.py \<display name\> \<server name (e.g. mastodon.example)\> \<access token\>`
+
+Example:
+
+```bash
+> python clock.py KizhiFox mastodon.ml qwertyuiopasdfghjklzxcvbnm1234567890
+🕦  11:43
+🕛  12:00
+🕧  12:30
+🕐  13:00
+🕜  13:30
+```
+
+To run this script you need to create an app with `write:accounts` permission.
---
+++ clock.py
@@ -0,0 +1,100 @@
+import sys
+import requests
+from datetime import datetime, timedelta
+from time import sleep
+
+
+class Clocks:
+    clock0 = '🕛'
+    clock0_30 = '🕧'
+    clock1 = '🕐'
+    clock1_30 = '🕜'
+    clock2 = '🕑'
+    clock2_30 = '🕝'
+    clock3 = '🕒'
+    clock3_30 = '🕞'
+    clock4 = '🕓'
+    clock4_30 = '🕟'
+    clock5 = '🕔'
+    clock_30 = '🕠'
+    clock6 = '🕕'
+    clock6_30 = '🕡'
+    clock7 = '🕖'
+    clock7_30 = '🕢'
+    clock8 = '🕗'
+    clock8_30 = '🕣'
+    clock9 = '🕘'
+    clock9_30 = '🕤'
+    clock10 = '🕙'
+    clock10_30 = '🕥'
+    clock11 = '🕚'
+    clock11_30 = '🕦'
+
+    def get_clock(self, hour: int, half: bool) -> str:
+        if hour < 1 or hour > 12:
+            raise ValueError
+        if half:
+            minutes = '_30'
+        else:
+            minutes = ''
+        return getattr(self, f'clock{hour}{minutes}')
+
+
+clocks = Clocks()
+
+
+def set_clock(display_name: str, server_name: str, access_token: str):
+    time = datetime.now()
+    hour = time.hour if time.hour < 12 else time.hour - 12
+    half = True if time.minute >= 30 else False
+
+    clock = clocks.get_clock(hour, half)
+
+    print(f'{clock}  {time.hour}:{time.minute}')
+
+    res = requests.patch(
+        f'https://{server_name}/api/v1/accounts/update_credentials',
+        headers={
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        },
+        json={
+            'display_name': f'{display_name} {clock}'
+        }
+    )
+    if res.status_code == 200:
+        pass
+    else:
+        print(res.text)
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        name = sys.argv[1]
+        server = sys.argv[2]
+        token = sys.argv[3]
+
+        set_clock(name, server, token)
+
+        while True:
+            try:
+                current_time = datetime.now()
+                next_time = current_time + timedelta(minutes=30)
+                if next_time.minute < 30:
+                    next_time.replace(minute=0)
+                else:
+                    next_time.replace(minute=0)
+
+                time_to_sleep = next_time - current_time
+                sleep(time_to_sleep.total_seconds())
+
+                set_clock(name, server, token)
+
+            except KeyboardInterrupt:
+                print('Bye!')
+                sys.exit(0)
+
+    else:
+        print('format: clock.py <display name> <server name (e.g. mastodon.example)> <access token>')
+        sys.exit(-1)
